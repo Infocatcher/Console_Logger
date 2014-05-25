Infocatcher/Console_Logger

commit 24f8107a307f3251d53dc6220496e65c1aef1fd6

Don't use cache for patterns.* preferences

bootstrap.js
@@ -304,7 +304,8 @@ var prefs = {
 			return;
 		var shortName = pName.substr(this.ns.length);
 		var val = this.getPref(pName);
-		this._cache[shortName] = val;
+		if(shortName.substr(0, 9) != "patterns.")
+			this._cache[shortName] = val;
 		consoleLogger.prefChanged(shortName, val);
 	},
 

commit 2f0b2a84b24561a8a56fe7a6bcda93616a7547f4

Fix: don't use cached preferences in loadPatterns() Regression in 24f8107a307f3251d53dc6220496e65c1aef1fd6 commit: we should keep cache updated or don't use it at all

bootstrap.js
@@ -146,7 +146,7 @@ var consoleLogger = {
 		return this.loadPatterns().excludes;
 	},
 	loadPatterns: function() {
-		var ns = "patterns.";
+		var ns = prefs.ns + "patterns.";
 		var patterns = { __proto__: null };
 		var messages = { __proto__: null };
 		var excludes = { __proto__: null };
@@ -154,10 +154,10 @@ var consoleLogger = {
 		var _patterns = { __proto__: null };
 		var _disabled = { __proto__: null };
 		var _excludes = { __proto__: null };
-		Services.prefs.getBranch(prefs.ns + ns)
+		Services.prefs.getBranch(ns)
 			.getChildList("", {})
 			.forEach(function(pName) {
-				var val = prefs.get(ns + pName);
+				var val = prefs.getPref(ns + pName);
 				var pShort, type;
 				if(/\.[^.]+$/.test(pName)) {
 					pShort = RegExp.leftContext;
@@ -209,7 +209,7 @@ var consoleLogger = {
 			patterns: (this.patterns = patterns),
 			messages: (this.messages = messages),
 			excludes: (this.excludes = excludes)
-		}
+		};
 	},
 
 	_files: { __proto__: null },