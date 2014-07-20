Infocatcher/Console_Logger

commit 76d293bb68be327ef9331048d7875313a5d45443

Optimize this.modified getter/setter (#3)

options.js
@@ -566,10 +566,14 @@ var consoleLoggerOptions = {
 	get optionsHash() {
 		return JSON.stringify(this.options);
 	},
+	_modified: false,
 	get modified() {
-		return !this.applyBtn.disabled;
+		return this._modified;
 	},
 	set modified(modified) {
+		if(modified == this._modified)
+			return;
+		this._modified = modified;
 		this.applyBtn.disabled = !modified;
 		var baseTitle = this._baseTitle || (this._baseTitle = document.title);
 		document.title = (modified ? "* " : "") + baseTitle;

commit 2cdd93ab5c1744e183a41dbd9db57da4f7346c87

Correctly set initial modified = false (regression in 76d293bb68be327ef9331048d7875313a5d45443 commit) (#3)

options.js
@@ -31,6 +31,7 @@ var consoleLoggerOptions = {
 		var root = document.documentElement;
 		var applyBtn = this.applyBtn = root.getButton("extra1");
 		applyBtn.setAttribute("icon", "apply");
+		applyBtn.disabled = true;
 		// Insert Apply button between OK and Cancel
 		var okBtn = root.getButton("accept");
 		var cancelBtn = root.getButton("cancel");