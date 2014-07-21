Infocatcher/Console_Logger

commit d9a45af09c59cc00041fede3fcfa865d37305e9f

Show "* " in window title, if options aren't saved (#3)

options.js
@@ -571,6 +571,8 @@ var consoleLoggerOptions = {
 	},
 	set modified(modified) {
 		this.applyBtn.disabled = !modified;
+		var baseTitle = this._baseTitle || (this._baseTitle = document.title);
+		document.title = (modified ? "* " : "") + baseTitle;
 	},
 	markAsSaved: function() {
 		this._savedOptions = this.optionsHash;

commit c89a51f306457c108249e130808ac733fd067b4e

Don't show "* " in save options confirmation (was broken in d9a45af09c59cc00041fede3fcfa865d37305e9f commit) (#14)

options.js
@@ -567,6 +567,10 @@ var consoleLoggerOptions = {
 	get optionsHash() {
 		return JSON.stringify(this.options);
 	},
+	get baseTitle() {
+		delete this.baseTitle;
+		return this.baseTitle = document.title;
+	},
 	_modified: false,
 	get modified() {
 		return this._modified;
@@ -576,8 +580,7 @@ var consoleLoggerOptions = {
 			return;
 		this._modified = modified;
 		this.applyBtn.disabled = !modified;
-		var baseTitle = this._baseTitle || (this._baseTitle = document.title);
-		document.title = (modified ? "* " : "") + baseTitle;
+		document.title = (modified ? "* " : "") + this.baseTitle;
 	},
 	markAsSaved: function() {
 		this._savedOptions = this.optionsHash;
@@ -688,7 +691,7 @@ var consoleLoggerOptions = {
 			if(
 				ps.confirmEx(
 					window,
-					document.title,
+					this.baseTitle,
 					strings.saveChanges,
 					ps.BUTTON_POS_0 * ps.BUTTON_TITLE_SAVE + ps.BUTTON_POS_1 * ps.BUTTON_TITLE_CANCEL,
 					"", "", "", null, {}