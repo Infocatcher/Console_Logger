29797c8ba37363e61c279ff2495800c96968b16f
Infocatcher <infocatcher.dev.null@gmail.com>
2015-12-12 22:48:28 +0300

    Add context menu item to invert selection

--- options.js
+++ options.js
@@ -64,6 +64,7 @@ var consoleLoggerOptions = {
 			// Note: works, if installed Console2 extension
 			this.$("cl-sep-beforeSelectAll").setAttribute("hidden", "true");
 			this.$("cl-mi-selectAll").setAttribute("hidden", "true");
+			this.$("cl-mi-invertSelection").setAttribute("hidden", "true");
 		}
 		if(!("timeout" in this.filter)) { // Firefox 3.0 and older
 			this.filter.setAttribute("type", "timed");
