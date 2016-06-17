Infocatcher/Console_Logger

commit da58478a858246abed2074d405f11972e62076bc

Add shortcut for profile directory

options.js
@@ -664,7 +664,7 @@ var consoleLoggerOptions = {
 		var _this = this;
 		var absPath = path.replace(/%([^%]+)%/g, function(s, alias) {
 			if(alias == "cl_ProfDrv")
-				return _this.getFileRoot(Services.dirsvc.get("ProfD", Components.interfaces.nsIFile)).path;
+				return _this.getFileRoot(this.cl.io.profileDir).path;
 			try {
 				return Services.dirsvc.get(alias, Components.interfaces.nsIFile).path;
 			}

io.js
@@ -73,11 +73,15 @@ var consoleLoggerIO = {
 	},
 
 	_files: { __proto__: null },
+	get profileDir() {
+		delete this.profileDir;
+		return this.profileDir = Services.dirsvc.get("ProfD", Components.interfaces.nsIFile);
+	},
 	getFile: function(key, name) {
 		var files = this._files;
 		if(key in files)
 			return files[key];
-		var file = Services.dirsvc.get("ProfD", Components.interfaces.nsIFile);
+		var file = this.profileDir.clone();
 		file.append(name || FILE_NAME_PREFIX + this.safeFileName(key) + ".log");
 		return files[key] = file;
 	},

commit 0f3274b2c085b8725b989e2c0ac67e88f7da8083

Fix typo: correctly expand %cl_ProfDrv% (introduced in da58478a858246abed2074d405f11972e62076bc commit)

options.js
@@ -664,7 +664,7 @@ var consoleLoggerOptions = {
 		var _this = this;
 		var absPath = path.replace(/%([^%]+)%/g, function(s, alias) {
 			if(alias == "cl_ProfDrv")
-				return _this.getFileRoot(this.cl.io.profileDir).path;
+				return _this.getFileRoot(_this.cl.io.profileDir).path;
 			try {
 				return Services.dirsvc.get(alias, Components.interfaces.nsIFile).path;
 			}