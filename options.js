1287fde3e4b891ba7d3c8954c70c4bf449eb407a
Infocatcher <infocatcher.dev.null@gmail.com>
2025-08-13 00:02:46 +0300

    Clarify: "Export/Copy All" should actually handle all items (not only visible ones)

--- options.js
+++ options.js
@@ -449,7 +449,7 @@ var consoleLoggerOptions = {
 	exportOptions: function(all) {
 		var options = { __proto__: null };
 		var items = all
-			? this.visibleItems
+			? this.items
 			: this.selectedItems;
 		items.forEach(function(cli) {
 			var item = cli.state;
@@ -957,9 +957,9 @@ var consoleLoggerOptions = {
 			this.$("cl-mi-opts-cut").setAttribute("disabled", cantReset);
 			this.$("cl-mi-copy").setAttribute("disabled", cantReset);
 			this.$("cl-mi-opts-copy").setAttribute("disabled", cantReset);
-			this.$("cl-mi-opts-copyAll").setAttribute("disabled", noVisible);
+			this.$("cl-mi-opts-copyAll").setAttribute("disabled", isEmpty);
 			this.$("cl-mi-opts-export").setAttribute("disabled", cantReset);
-			this.$("cl-mi-opts-exportAll").setAttribute("disabled", noVisible);
+			this.$("cl-mi-opts-exportAll").setAttribute("disabled", isEmpty);
 			this.$("cl-mi-opts-importOvr").setAttribute("disabled", isEmpty);
 		}, this);
 	},
